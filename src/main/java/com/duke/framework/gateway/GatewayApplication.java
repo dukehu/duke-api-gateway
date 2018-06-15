204b31c86413fb087ba77d329508bb447c756dc4
duke <duke0417@foxmail.com>
2018-06-15 14:11:12 +0800

    新建服务网关项目

--- src/main/java/com/duke/framework/gateway/GatewayApplication.java
+++ src/main/java/com/duke/framework/gateway/GatewayApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
 /**
- * Created by ZHANGWEI5095@QQ.COM on 14:09 2017/6/5.
+ * Created duke on 2018/6/14
  */
 @SpringBootApplication
 @EnableZuulProxy
