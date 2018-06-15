6d51ff4e880cef465a331dc8b927bab021597ebd
duke <duke0417@foxmail.com>
2018-06-15 09:12:08 +0800

    新建服务网关项目

---
+++ src/main/java/com/duke/framework/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.duke.framework.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * Created by ZHANGWEI5095@QQ.COM on 14:09 2017/6/5.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
